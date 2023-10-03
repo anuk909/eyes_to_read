be800c495046b6f35846a9e1eb46ba78d59115df
Shmulik Cohen <anuk909@gmail.com>
2023-10-03 15:27:31 +0300

    small lints

--- eyes_to_read_be/eyes_to_read_be/routes/user_routes.py
+++ eyes_to_read_be/eyes_to_read_be/routes/user_routes.py
@@ -13,19 +13,20 @@ db = client["eyes_to_see_db"]
 users_collection = db["users"]
 
 
-
-
 @user_router.post()
 async def add_user(user: User):
     # Check if the user exists
     existing_user = users_collection.find_one({"name": user.name})
     if existing_user:
-        raise HTTPException(status_code=400, detail="A user with this name already exists.")
-    
+        raise HTTPException(
+            status_code=400, detail="A user with this name already exists."
+        )
+
     # Add user
     users_collection.insert_one(user.model_dump)
 
     return {"message": "User data updated successfully"}
+
 
 @user_router.put("/{user_id}")
 async def update_user(user_id: str, user: User):
